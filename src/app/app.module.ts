40f9ebdd3f4d308e45625c4f875b8ca22d0849cf
RafaeJon <rafaeljonathanpt@gmail.com>
2020-09-23 00:02:35 +0700

    added filter box clear container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { JobItemComponent } from './home/job-item/job-item.component';
 import { FilterItemComponent } from './home/job-item/filter-item/filter-item.component';
+import { FilterBoxComponent } from './home/filter-box/filter-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     JobItemComponent,
-    FilterItemComponent
+    FilterItemComponent,
+    FilterBoxComponent
   ],
   imports: [
     BrowserModule
