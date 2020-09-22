03fa37e8209de2db965a0d695ff30583ceed2bd0
RafaeJon <rafaeljonathanpt@gmail.com>
2020-09-22 22:28:10 +0700

    finish addding filter item component and some animation on hover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { JobItemComponent } from './home/job-item/job-item.component';
+import { FilterItemComponent } from './home/job-item/filter-item/filter-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    JobItemComponent
+    JobItemComponent,
+    FilterItemComponent
   ],
   imports: [
     BrowserModule
